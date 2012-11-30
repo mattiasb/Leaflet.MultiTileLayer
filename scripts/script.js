88aa6b5e6eef4ad9387c4d3027782671d1c24ad8
Mattias Bengtsson <mattias.jc.bengtsson@gmail.com>
2012-11-30 06:50:10 +0100

    Make the page actually work ok

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+var map = L.map('map').setView(L.latLng(57.7, 11.9), 12);
+
+var cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade';
+
+L.TileLayer.multi({
+	14: {
+		url: 'http://otile{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png',
+		subdomains:'1234'
+	},
+	17: {
+		url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+	}
+}, {
+	minZoom: 0,
+	maxZoom: 17,
+	attribution: 'Tiles Courtesy of <a href="http://www.mapquest.com/">MapQuest</a> &mdash; '
+		+ '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, '
+		+ '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+		+  '&mdash; '
+		+ 'Tiles &copy; Esri &mdash; '
+		+ 'Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, '
+		+ 'Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+}).addTo(map);
